0863158857a24b5aa524e8d16e83765122a00dcf
Piotr <piteeroo@yahoo.com>
2016-01-28 00:30:43 +0100

    Create CalculationScanner.java

---
+++ Calculation/CalculationScanner.java
@@ -0,0 +1,12 @@
+package com.CarWash.Calculation;
+
+import java.util.Scanner;
+
+public class CalculationScanner
+{
+	public static Scanner scanner()
+	{
+		Scanner scanner = new Scanner(System.in);
+		return scanner;
+	}
+}
