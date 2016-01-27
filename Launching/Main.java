c7cfe2fd601018fadcf05ca182265b0371391201
Piotr <piteeroo@yahoo.com>
2016-01-28 00:33:57 +0100

    Create Main.java

---
+++ Launching/Main.java
@@ -0,0 +1,18 @@
+package com.CarWash.Launching;
+
+import com.CarWash.Calculation.Options;
+import com.CarWash.Calculation.Repeat;
+
+public class Main
+{
+	public static void main(String[] args) throws Exception
+	{
+
+		
+		Options.options();
+		Repeat.repeat();
+		
+		
+		
+	}
+}
